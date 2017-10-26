d0aaa9f37da07720e5eb672c196f9be69499a53b
athompson909 <athompson909@gmail.com>
2017-10-26 13:53:53 -0600

    successfully scraped DC libraries events data

--- dc_libs/dc_libraries.py
+++ dc_libs/dc_libraries.py
@@ -4,9 +4,19 @@ from __future__ import print_function
 from bs4 import BeautifulSoup
 import requests
 import csv
-import nltk
+# import nltk
 import re
+import pandas as pd
 
+
+# ######################################################################
+#  This is for all Washington DC public libraries, (managed by Drupal)
+# ######################################################################
+# - all DC libraries are managed by the same Drupal system, making it
+#   easy to get the data from all the websites of DC at once
+# - I wonder if there will be other Drupal websites like this and if
+#   this file could be a template for them
+# ######################################################################
 
 def print_element(elem):
     print('__________________________________________________')
@@ -20,8 +30,7 @@ def print_list(l):
 
 
 # first get all the links
-def get_links_from_url():
-    url = 'https://www.dclibrary.org'
+def get_links_from_url(url):
     res = requests.get(url)
     html = res.content
     soup = BeautifulSoup(html, 'lxml')
@@ -37,7 +46,7 @@ def get_links_from_url():
         links.append(url + link.find('a', href=True)['href'])
 
     # print_list(links)
-    with open("links.csv", 'wb') as f:
+    with open('links.csv', 'wb') as f:
         wr = csv.writer(f)
         wr.writerow(links)
 
@@ -52,8 +61,49 @@ def get_list(file_name):
     return l[0]
 
 
+def get_events(links, p_lib, b_url):
+    c = 0
+    df = pd.DataFrame()
+    for url in links:
+        res = requests.get(url)
+        html = res.content
+        soup = BeautifulSoup(html, 'lxml')
+
+        library = p_lib + ' - ' + soup.select('#page-title')[0].text  # I can do this cuz it's an id
+        # if there's an error it's a problem with their being two of the same id in the html
+
+        events = soup.select('#eventsTab')
+        if len(events) > 0:
+            rows = events[0].select('.row')
+            title = link = date = description = ''
+            for row in rows:
+                h = row.select('.field-name-title')[0]
+
+                link = b_url + h.find('a', href=True)['href']
+                title = h.text
+                dts = row.select('.date-display-single')
+                if len(dts) > 0:
+                    date = dts[0].text
+                description = row.select('.field-name-body')[0].text
+                df = df.append({
+                    'Library': library,
+                    'Title': title,
+                    'Link': link,
+                    'Date': date,
+                    'Description': description},
+                    ignore_index=True)
+
+        # if c > 0: break  # TODO: remove
+        c += 1
+        # print('__________________________________________________')
+    print(df)
+    df.to_csv('dc_libs_events.csv', sep='\t', encoding='utf-8')
+
+
 # then go through and take out the needed data from that piece of the page
 # links = get_links_from_url()
-links = get_list('links.csv')
-# print_list(links)
-
+parent = 'Washington DC'
+base_url = 'https://www.dclibrary.org'
+l = get_list('links.csv')  # getting list of links
+# print_list(l)
+get_events(l, parent, base_url)
